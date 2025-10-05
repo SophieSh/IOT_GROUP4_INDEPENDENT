ede47db4c8c9df3eca77a2b8d4164eadc9c258d5
Sophie <sofishtern@gmail.com>
2025-10-05 19:34:06 +0300

    Remove c compilation fix, convert C to CPP

--- progress_bar/timer_ui.h
+++ progress_bar/timer_ui.h
@@ -3,16 +3,7 @@
 
 #include <lvgl.h>
 
-// This block tells the C++ compiler to use C naming conventions for the functions inside.
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// Function prototype: tells the main file this function is defined in timer_ui.c
+// Function prototype: tells the main file this function exists
 void create_timer_bar(int duration_seconds); 
 
-#ifdef __cplusplus
-}
 #endif
-
-#endif // TIMER_UI_H
