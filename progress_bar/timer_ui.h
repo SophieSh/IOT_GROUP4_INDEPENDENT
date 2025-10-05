56273ff9ad5d3c31190c664c082269b90c29eb71
Sophie <sofishtern@gmail.com>
2025-10-05 19:26:34 +0300

    Fix: Resolve linker error for create_timer_bar(int) by enforcing C linkage in timer_ui.h.

--- progress_bar/timer_ui.h
+++ progress_bar/timer_ui.h
@@ -3,7 +3,16 @@
 
 #include <lvgl.h>
 
+// This block tells the C++ compiler to use C naming conventions for the functions inside.
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // Function prototype: tells the main file this function is defined in timer_ui.c
 void create_timer_bar(int duration_seconds); 
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif // TIMER_UI_H
